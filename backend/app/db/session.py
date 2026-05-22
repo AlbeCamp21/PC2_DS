0ec72b42b05b4b42e2bcff75e8ff4dbc6c2cb2a3
AlbeCamp21 <luis.alanya.c@uni.pe>
2026-05-22 10:58:03 -0500

    PC2: agregar patrón Singleton

--- backend/app/db/session.py
+++ backend/app/db/session.py
@@ -6,14 +6,33 @@ from dotenv import load_dotenv
 load_dotenv()
 DATABASE_URL = os.getenv("DATABASE_URL_LOCAL")
 
-engine = create_engine(DATABASE_URL)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+class DatabaseSingleton:
+    _instance = None
+
+    def __new__(cls):
+        # Instancia patron de diseño singleton
+        if cls._instance is None:
+            cls._instance = super(DatabaseSingleton, cls).__new__(cls)
+            cls._instance._engine = create_engine(DATABASE_URL)
+            cls._instance._SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=cls._instance._engine)
+        return cls._instance
+
+    @property
+    def engine(self):
+        # Retorna motor de base datos
+        return self._engine
+
+    @property
+    def SessionLocal(self):
+        # Retorna fabricante de sesiones
+        return self._SessionLocal
+
+db_instance = DatabaseSingleton()
+SessionLocal = db_instance.SessionLocal
+engine = db_instance.engine
 
 def get_db():
-    """
-    Generador de sesiones para ser usado en los scripts o FastAPI.
-    Asegura que la sesión se cierre después de usarse.
-    """
+    # Retorna sesion de base datos
     db = SessionLocal()
     try:
         yield db
